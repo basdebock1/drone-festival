f5cf282be581924a1400fca48f9279c73c38dd0f
Bas De Bock <bas.debock@student.kdg.be>
2023-01-30 22:07:43 +0100

    Added dynamic programma tab navigation

--- snippets/components/programma-menu.php
+++ snippets/components/programma-menu.php
@@ -1,10 +1,12 @@
+<?php $activeTab = basename($_SERVER['PHP_SELF'], ".php"); ?>
+
 <div class="programma-navigation">
     <div class="programma-links">
         <ul class="programma-list">
-            <li class="programma-item active"><a href="programma.php" class="nav-link">Overzicht</a></li>
-            <li class="programma-item"><a href="programma-vrijdag.php" class="nav-link">Vrijdag</a></li>
-            <li class="programma-item"><a href="programma-zaterdag.php" class="nav-link">Zaterdag</a></li>
-            <li class="programma-item"><a href="programma-droneshows.php" class="nav-link">Drone Shows</a></li>
+            <li class="programma-item <?= $activeTab == 'programma' ? 'active' : '' ?>"><a href="programma.php" class="nav-link">Overzicht</a></li>
+            <li class="programma-item <?= $activeTab == 'programma-vrijdag' ? 'active' : '' ?>"><a href="programma-vrijdag.php" class="nav-link">Vrijdag</a></li>
+            <li class="programma-item <?= $activeTab == 'programma-zaterdag' ? 'active' : '' ?>"><a href="programma-zaterdag.php" class="nav-link">Zaterdag</a></li>
+            <li class="programma-item <?= $activeTab == 'programma-droneshows' ? 'active' : '' ?>"><a href="programma-droneshows.php" class="nav-link">Drone Shows</a></li>
         </ul>
     </div>
 </div>
