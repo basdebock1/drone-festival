c746387643452192578c07e5baec331146b8edb0
Bas De Bock <bas.debock@student.kdg.be>
2023-01-31 01:30:12 +0100

    Made cards for artists more dynamic
    + Cards via foreach loop met if statement
    - Card OOP nog op te lossen
    - Error in programma verder testen en afwerken

---
+++ config/app.php
@@ -0,0 +1,51 @@
+<?php $activeTab = basename($_SERVER['PHP_SELF'], ".php");
+
+$artist = 0;
+$day = 1;
+$hour = 2;
+$type = 3;
+$img = 4;
+$programmas = [
+    [
+        "Lights Out",
+        "Zaterdag",
+        "22u00",
+        "lazer",
+        "drone\ lazer.jpg",
+    ],
+    [
+        "Team Lazer BEAM",
+        "Vrijdag",
+        "23u00",
+        "lazer",
+        "team\ lazer\ beam.jpg"
+    ],
+    [
+        "Cesar",
+        "Vrijdag",
+        "20u00",
+        "band",
+        "cesar.jpg"
+    ],
+    [
+        "Erik Hartman",
+        "Zaterdag",
+        "20u00",
+        "band",
+        "erik\ hartman.jpg"
+    ],
+    [
+        "Frankie Loosveld",
+        "Vrijdag",
+        "22u00",
+        "band",
+        "frankie\ loosveld.jpg"
+    ],
+    [
+        "VJ Tony",
+        "Zaterdag",
+        "23u00",
+        "dj",
+        "drone\ dj.jpg"
+    ],
+];
